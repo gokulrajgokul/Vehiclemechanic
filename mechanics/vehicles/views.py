e059da15170f1891a31b3c992f10065b7a58182c
saran <saranvms2002@gmail.com>
2025-07-17 19:37:50 +0530

    srted

--- mechanics/vehicles/views.py
+++ mechanics/vehicles/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render,redirect
 from django.contrib import admin
 from django.urls import path, include   
 
-# Create your views here.
-def home(request):
-    return render(request, 'base.html')
-
 
 def home(request):
     return render(request,'home.html')
